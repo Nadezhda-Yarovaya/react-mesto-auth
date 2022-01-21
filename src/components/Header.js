c65934f8ad42e3a1cb0cbba14f210c3954aaf42d
Nadezhda Yarovaya <seriozz@yandex.ru>
2022-01-22 00:00:18 +0300

    header in new branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import mainLogo from '../images/mainlogo.svg';
 import { Link, useLocation } from 'react-router-dom';
+import { Route, Switch, } from 'react-router-dom';
+import ProtectedRoute from "./ProtectedRoute";
 
 const Header = (props) => {
   const location = useLocation();
+
+  /*
+  {props.loggedIn ? (
+          <>
+            <p className="header__email">{props.headerEmail}</p><button onClick={props.signOut} className="header__login-button">Выйти</button>
+          </>
+        ) :
+          (
+            <Link to={location.pathname === '/signin' ? './sign-up' : './signin'} className="header__link-button">{location.pathname === '/signin' ? 'Регистрация' : 'Войти'}</Link>
+          )
+
+        }
+        */
 
   return (
     <header className="header page__header">
@@ -14,16 +29,12 @@ const Header = (props) => {
       </Link>
 
       <div className="header__logged-in-info">
-        {props.loggedIn ? (
-          <>
-            <p className="header__email">{props.headerEmail}</p><button onClick={props.signOut} className="header__login-button">Выйти</button>
-          </>
-        ) :
-          (
-            <Link to={location.pathname === '/signin' ? './sign-up' : './signin'} className="header__link-button">{location.pathname === '/signin' ? 'Регистрация' : 'Войти'}</Link>
-          )
-
-        }
+      <Switch>
+              <Route path="/signin">Регистрация</Route>
+              <Route path="/sign-up">ВХОД</Route>
+              <ProtectedRoute path="/" loggedIn={props.loggedIn}>пишу что хочу</ProtectedRoute>
+              </Switch>
+        
       </div>
     </header>
   );
